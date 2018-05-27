3ca88fbf11cdbd149238f744ad565a14340029d9
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-27 12:12:12 +0200

    add tasks controller

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the tasks
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+}
